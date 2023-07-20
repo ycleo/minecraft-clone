40aa1c44d87a801a1d6c5f411133c3068ca02c17
ycleo <leoyclee@gmail.com>
2023-07-20 23:36:56 +0800

    save world to local storage feature

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,8 +1,10 @@
 import { useBox } from "@react-three/cannon"
 import * as textures from "../images/textures"
 import { useStore } from "../hooks/useStore"
+import { useState } from "react"
 
 export const Cube = ({ position, texture }) => {
+    const [isHovered, setIsHovered] = useState(false)
     const [ref] = useBox(() => ({
         type: 'static',
         position
@@ -13,42 +15,55 @@ export const Cube = ({ position, texture }) => {
     const activeTexture = textures[texture + "Texture"]
     return (
         <mesh 
-        onClick={(e) => {
-            e.stopPropagation()
-            const clickedFace = Math.floor(e.faceIndex / 2)
-            const { x, y, z } = ref.current.position
-            if (e.altKey) {
-                removeCube(x, y, z)
-                return
-            }
-            else if (clickedFace === 0) {
-                addCube(x + 1, y, z)
-                return
-            }
-            else if (clickedFace === 1) {
-                addCube(x - 1, y, z)
-                return
-            }
-            else if (clickedFace === 2) {
-                addCube(x, y + 1, z)
-                return
-            }
-            else if (clickedFace === 3) {
-                addCube(x, y - 1, z)
-                return
-            }
-            else if (clickedFace === 4) {
-                addCube(x, y, z + 1)
-                return
-            }
-            else if (clickedFace === 5) {
-                addCube(x, y, z - 1)
-                return
-            }
-        }}
+            onPointerMove={(e) => {
+                e.stopPropagation()
+                setIsHovered(true)
+            }}
+            onPointerOut={(e) => {
+                e.stopPropagation()
+                setIsHovered(false)
+            }}
+            onClick={(e) => {
+                e.stopPropagation()
+                const clickedFace = Math.floor(e.faceIndex / 2)
+                const { x, y, z } = ref.current.position
+                if (e.altKey) {
+                    removeCube(x, y, z)
+                    return
+                }
+                else if (clickedFace === 0) {
+                    addCube(x + 1, y, z)
+                    return
+                }
+                else if (clickedFace === 1) {
+                    addCube(x - 1, y, z)
+                    return
+                }
+                else if (clickedFace === 2) {
+                    addCube(x, y + 1, z)
+                    return
+                }
+                else if (clickedFace === 3) {
+                    addCube(x, y - 1, z)
+                    return
+                }
+                else if (clickedFace === 4) {
+                    addCube(x, y, z + 1)
+                    return
+                }
+                else if (clickedFace === 5) {
+                    addCube(x, y, z - 1)
+                    return
+                }
+            }}
         ref={ref}>
             <boxBufferGeometry attach="geometry" />
-            <meshStandardMaterial map={activeTexture} attach="material" />
+            <meshStandardMaterial 
+                color={isHovered ? 'grey' : 'white'}
+				map={activeTexture}
+				transparent={true}
+				opacity={texture === 'glass' ? 0.6 : 1} 
+                attach="material" />
         </mesh>
     )
 }
