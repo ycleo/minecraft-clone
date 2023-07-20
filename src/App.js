5325f598ea2f9cf30a9ca7f08cc6a8a4924be11a
ycleo <leoyclee@gmail.com>
2023-07-20 18:46:40 +0800

    create Ground and Player components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Canvas } from '@react-three/fiber';
 import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
+import { Ground } from './components/Ground';
+import {Player } from './components/Player';
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
         <Sky sunPosition={[100, 200, 20]} />
         <ambientLight intensity={0.5} />
         <Physics>
-          
+          <Ground />
+          <Player />
         </Physics>
       </Canvas>
     </>
