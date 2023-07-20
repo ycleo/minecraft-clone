357268c9af302828cda7e04ddebe02f1f4d88d2e
ycleo <leoyclee@gmail.com>
2023-07-20 21:56:56 +0800

    cubes and cube component

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,5 +1,4 @@
 import { usePlane } from "@react-three/cannon";
-import { NearestFilter, RepeatWrapping } from "three";
 import { groundTexture } from "../images/textures";
 
 export const Ground = () => {
@@ -7,11 +6,8 @@ export const Ground = () => {
         rotation: [-Math.PI / 2, 0, 0], position: [0, -0.5, 0]
     }))
 
-    groundTexture.magFilter = NearestFilter
-    groundTexture.wrapS = RepeatWrapping
-    groundTexture.wrapT = RepeatWrapping
-    groundTexture.repeat.set(100, 100)
-
+    groundTexture.repeat.set(100, 100);
+    
     return (
         <mesh ref={ref}>
             <planeBufferGeometry attach='geometry' args={[100, 100]} />
