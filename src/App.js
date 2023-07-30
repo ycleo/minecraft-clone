95050ae07c9fcc5f91cade38d00f9fa718ec0502
ycleo <leoyclee@gmail.com>
2023-07-30 20:29:42 +0800

    add outline

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Canvas } from '@react-three/fiber';
 import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 import { Ground } from './components/Ground';
-import {Player } from './components/Player';
+import { Player } from './components/Player';
 import { FPV } from './components/FPV';
 import { Cubes } from './components/Cubes';
 import { TextureSelector } from './components/TextureSelector';
